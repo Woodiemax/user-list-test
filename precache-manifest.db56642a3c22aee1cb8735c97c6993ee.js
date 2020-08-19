1378bc3f782eae802741dd38579eb6d9055771fc
Pinhead <Woodiemax1989@gmail.com>
2020-08-19 23:40:59 +0300

    Updates

---
+++ precache-manifest.db56642a3c22aee1cb8735c97c6993ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d0b04deb7c98fe6dc016b828a85c75c",
+    "url": "/user-list-test/index.html"
+  },
+  {
+    "revision": "e7e132ceafedd01ea3c5",
+    "url": "/user-list-test/static/css/main.55d70535.chunk.css"
+  },
+  {
+    "revision": "4ff234dc6bb74640dd8f",
+    "url": "/user-list-test/static/js/2.49487ddc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user-list-test/static/js/2.49487ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e132ceafedd01ea3c5",
+    "url": "/user-list-test/static/js/main.d19fa5b3.chunk.js"
+  },
+  {
+    "revision": "f8d427198edaa88cca52",
+    "url": "/user-list-test/static/js/runtime-main.df99e391.js"
+  }
+]);
